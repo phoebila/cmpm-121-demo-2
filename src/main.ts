1de11227394602c3187fcc9f9d9dd5f0bf65bfe4
Phoebe <128556139+phoebila@users.noreply.github.com>
2024-10-15 10:26:46 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -16,3 +16,41 @@ canvas.width = 256;
 canvas.height = 256;
 canvas.id = "sticker-canvas"; //css id for styling
 app.appendChild(canvas);
+
+// Step 2 - Clear Button and Draw Handler -------------------------------------
+const clearButton = document.createElement("button");
+clearButton.textContent = "Clear";
+clearButton.id = "clear-button";
+app.appendChild(clearButton);
+
+// Getting canvas context for drawing
+const ctx = canvas.getContext("2d")!;
+let isDrawing = false;
+
+// mouse handling
+canvas.addEventListener("mousedown", (e) => {
+    isDrawing = true;
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX, e.offsetY); //starting at mouse location
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if (isDrawing) {
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+    }
+});
+
+canvas.addEventListener("mouseup", () => {
+    isDrawing = false;
+    ctx.closePath();
+});
+
+canvas.addEventListener("mouseout", () => {
+    isDrawing = false;
+});
+
+// clear button handler
+clearButton.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
