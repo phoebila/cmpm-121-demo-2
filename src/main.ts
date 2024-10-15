0b6043bd3452193ab720ccef9d82714504b1cc57
Phoebe <128556139+phoebila@users.noreply.github.com>
2024-10-15 10:13:52 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,15 @@ const APP_NAME = "Sticker App";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+// Step 1 - H1 element with text "Sticker App" -------------------------
+const heading = document.createElement("h1");
+heading.textContent = APP_NAME;
+app.appendChild(heading);
+
+// Creating Canvas Element
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "sticker-canvas"; //css id for styling
+app.appendChild(canvas);
